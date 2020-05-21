8eab802e011f88feda0847c5c5ea0f17da133f0c
Eduardo Martínez <eduardo.mzhz@gmail.com>
2020-05-20 20:42:05 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    hello world!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return { }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
